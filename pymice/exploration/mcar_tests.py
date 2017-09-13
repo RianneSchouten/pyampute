43a3cb8a1f1364c5e226c920e4cf263f2a4bdbbb
Rianne Schouten <rianne.schouten@dpa.nl>
2017-09-13 10:15:08 +0200

    make mcar_tests.py available

---
+++ pymice/exploration/mcar_tests.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+import math as ma
+import scipy.stats as st
+
+class McarTests():
+
+    def __init__(self, data):
+        self.data = data
+
+    def mcar_test(self):
+        """ Implementation of Little's MCAR test
+
+        Parameters
+        ----------
+        data: Pandas DataFrame
+            An incomplete dataset with samples as index and variables as columns
+
+        Returns
+        -------
+        p_value: Float
+            This value is the outcome of a chi-square statistical test, testing whether the null hypothesis
+            'the missingness mechanism of the incomplete dataset is MCAR' can be rejected.
+        """
+
+        dataset = self.data
+        vars = dataset.dtypes.index.values
+        n_var = dataset.shape[1]
+
+        # mean and covariance estimates
+        # ideally, this is done with a maximum likelihood estimator
+        gmean = dataset.mean()
+        gcov = dataset.cov()
+
+        # set up missing data patterns
+        r = 1 * dataset.isnull()
+        mdp = np.dot(r, list(map(lambda x: ma.pow(2, x), range(n_var))))
+        sorted_mdp = sorted(np.unique(mdp))
+        n_pat = len(sorted_mdp)
+        correct_mdp = list(map(lambda x: sorted_mdp.index(x), mdp))
+        dataset['mdp'] = pd.Series(correct_mdp, index=dataset.index)
+
+        # calculate statistic and df
+        pj = 0
+        d2 = 0
+        for i in range(n_pat):
+            dataset_temp = dataset.loc[dataset['mdp'] == i, vars]
+            select_vars = ~dataset_temp.isnull().any()
+            pj += np.sum(select_vars)
+            select_vars = vars[select_vars]
+            means = dataset_temp[select_vars].mean() - gmean[select_vars]
+            select_cov = gcov.loc[select_vars, select_vars]
+            mj = len(dataset_temp)
+            parta = np.dot(means.T, np.linalg.solve(select_cov, np.identity(select_cov.shape[1])))
+            d2 += mj * (np.dot(parta, means))
+
+        df = pj - n_var
+
+        # perform test and save output
+        p_value = 1 - st.chi2.cdf(d2, df)
+
+        return p_value
+
+    def mcar_t_tests(self):
+        """ MCAR tests for each pair of variables
+
+        Parameters
+        ----------
+        data: Pandas DataFrame
+            An incomplete dataset with samples as index and variables as columns
+
+        Returns
+        -------
+        mcar_matrix: Pandas DataFrame
+            A square Pandas DataFrame containing True/False for each pair of variables
+            True: Missingness in index variable is MCAR for column variable
+            False: Missingness in index variable is not MCAR for column variable
+        """
+
+        dataset = self.data
+        vars = dataset.dtypes.index.values
+        mcar_matrix = pd.DataFrame(data=np.zeros(shape=(dataset.shape[1], dataset.shape[1])),
+                                   columns=vars, index=vars)
+
+        for var in vars:
+            for tvar in vars:
+                part_one = dataset.loc[dataset[var].isnull(), tvar]
+                part_two = dataset.loc[~dataset[var].isnull(), tvar]
+                mcar_matrix.loc[var, tvar] = st.ttest_ind(part_one, part_two, nan_policy='omit').pvalue
+
+        mcar_matrix = mcar_matrix[mcar_matrix.notnull()] > 0.05
+
+        return mcar_matrix
+
+
