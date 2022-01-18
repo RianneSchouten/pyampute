b172cdcc95c42ed7cd70174cbc6d41f6e3c850e3
RianneSchouten <rianneschouten@live.nl>
2022-01-18 12:41:53 +0100

    restructure revamp

---
+++ examples/introduction.py
@@ -0,0 +1,46 @@
+"""
+========================================
+Main title of example
+========================================
+Some explanation
+"""
+
+# %%
+# Author: Rianne Schouten <r.m.schouten@tue.nl>
+#
+# Synthetic dataset
+# -----------------
+#
+# Some explanation with a reference [1]_
+
+import numpy as np
+
+m = 1000
+n = 10
+com_dataset = np.zeros((m,n))
+
+# %%
+# Multivariate Amputation
+# -----------------------
+#
+# Some explanation
+
+from pymice.amputation.ampute import MultivariateAmputation
+
+mads = MultivariateAmputation()
+incomplete_data = mads.fit_transform(com_dataset)
+
+# %%
+
+from pymice.exploration.md_patterns import mdPatterns
+
+mdp = mdPatterns()
+plot = mdp._get_patterns(incomplete_data)
+
+# %%
+# References
+# ----------
+#
+# .. [1] `Generating missing values ...
+#        <https://www.tandfonline.com/doi/full/10.1080/00949655.2018.1491577>`_,
+#        Rianne M. Schouten, Peter Lugtig & Gerko Vink, etc.
