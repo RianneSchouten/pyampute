29483edbc0cf8a85cc3124adec445490c632950c
prabhant <prabhantsingh@gmail.com>
2022-02-07 15:03:54 +0100

    setup py mainufest and gitignore changes

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 from distutils.core import setup
+import setuptools
 
 setup(
     name="pyampute",
     version="0.0.1",
     description="Amputation suite based on the R MICE package.",
-    author=["Rianne Schouten", "Davina Zamanzadeh", "Prabhant Singh"],
-    author_email=["r.m.schouten@tue.nl", "davzaman@gmail.com", "p.singh@tue.nl"],
-    packages=["pyampute"],
+    author="Rianne Schouten,Davina Zamanzadeh,Prabhant Singh",
+    author_email="r.m.schouten@tue.nl,davzaman@gmail.com,p.singh@tue.nl",
+    packages=setuptools.find_packages(
+        include=["pyampute.*", "pyampute"], exclude=["*.tests", "*.tests.*", "tests.*", "tests"],
+    ),
     install_requires=[
         # "rich",  # nice stack traces + printing
         "pandas",
