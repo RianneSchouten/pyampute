0f29cfe93d6a3c4472e4863287e8c1780fe8f226
Davina Zaman <davzaman@gmail.com>
2022-07-18 00:23:01 -0700

    update metadata for pypi

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name="pyampute",
     version="0.0.2",
     description="Transformer for generating multivariate missingness in complete datasets",
+    long_description="Amputation is the opposite of imputation; it is the creation of a missing data mask for complete datasets. Amputation is useful for evaluating the effect of missing values on the outcome of a statistical or machine learning model. ``pyampute`` is the first open-source Python library for data amputation. Our package is compatible with the scikit-learn-style fit and transform paradigm, which allows for seamless integration of amputation in a larger, more complex data processing pipeline.",
     author="Rianne Schouten,Davina Zamanzadeh,Prabhant Singh",
     author_email="r.m.schouten@tue.nl,davzaman@gmail.com,p.singh@tue.nl",
     packages=setuptools.find_packages(
@@ -28,4 +29,9 @@ setup(
             "sphinx-gallery",
         ]
     },
+    project_urls={
+        "Documentation": "https://rianneschouten.github.io/pyampute/build/html/index.html",
+        "Source Code": "https://github.com/RianneSchouten/pyampute",
+    },
+    license="BSD",
 )
